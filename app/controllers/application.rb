359a320657c61b54b671b8f91e8dc999d2d4e458
Michael Murray <mdmurray@gmail.com>
2008-04-08 22:13:27 -0700

    Disabling application install for testing and ignoring amazon s3

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -8,7 +8,8 @@ class ApplicationController < ActionController::Base
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'e05a0ec6beed23fe8c5eee2dc035f455'
   
-  before_filter :ensure_application_is_installed_by_facebook_user
+  #before_filter :ensure_application_is_installed_by_facebook_user
+  
   helper_method :facebook_user
   def facebook_user
     @facebook_user ||= (session[:facebook_session] && session[:facebook_session].user) || :false
