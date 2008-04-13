89e2412f8b549ae36de4ac688493ddf0ee2f5815
Michael Murray <mdmurray@gmail.com>
2008-04-13 15:37:14 -0700

    New deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,6 @@
 set :keep_releases, 3
-set :application, "apicaday"
+set :application, "sudothinker"
 
-default_run_options[:pty] = true
 set :scm, :git
 set :user, "mik"
 set :repository,  "git@github.com:sudothinker/pic-a-day.git"
@@ -16,7 +15,7 @@ set :deploy_to, "/home/mik/www/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-set :location, 'pseudothinker.com'
+set :location, 'sudothinker.com'
 role :app, location
 role :web, location
 role :db,  location, :primary => true
