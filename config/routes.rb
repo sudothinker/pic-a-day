f0f4be29beb72eeed413b95dcb001d1e8395cf29
Michael Murray <mdmurray@gmail.com>
2008-04-15 22:11:20 -0700

    Route configuration with canvas

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
-  #map.with_options :conditions => {:canvas => true} do |c|
-  map.home '', :controller => "pictures"
-  #end
-  map.capture '/capture', :controller => "pictures", :action => "capture"
-  map.resources :pictures
+  map.with_options :conditions => {:canvas => true} do |c|
+    c.with_options :controller => 'pictures' do |f|
+      f.home ''
+      f.capture '/capture', :action => "capture"    
+    end
+    c.resources :pictures
+  end
+  
   # Install the default routes as the lowest priority.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
