58f77e3bcaffa0aa208983799c827d44ea67f022
Michael Murray <mdmurray@gmail.com>
2008-04-22 21:12:55 -0700

    Updating profile fbml with profile pic

--- lib/user_mixin.rb
+++ lib/user_mixin.rb
@@ -7,9 +7,9 @@ module Facebooker
       Digest::SHA1.hexdigest("--#{user_id}--70e167fcbe5217ea2250b552d13a4f675a6a4993--")
     end
     
-    def set_profile_fbml!(picture)
-      puts "Updating fbml for #{self.id}"
-      parameters = {:uid => self.id}
+    def self.set_profile_fbml!(user_id, picture)
+      puts "Updating fbml for #{user_id}"
+      parameters = {:uid => user_id}
       fbml = <<-FBML
         <img src="#{picture.profile.authenticated_s3_url}" alt="Me" />
       FBML
