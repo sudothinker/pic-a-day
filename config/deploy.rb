32c8b331d62ba1a5f773864e7aebc98d6f5ac113
jesse <jesse@166.48.222.10.in-addr.arpa>
2008-05-18 12:46:31 -0700

    updating styles

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :keep_releases, 3
-set :application, "apicaday"
+set :application, "stage.apicaday"
 
 set :scm, :git
 set :user, "mik"
@@ -15,7 +15,7 @@ set :deploy_to, "/home/mik/www/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-set :location, 'pseudothinker.com'
+set :location, 'stage.pseudothinker.com'
 role :app, location
 role :web, location
 role :db,  location, :primary => true
