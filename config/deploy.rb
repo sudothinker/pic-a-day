e4ebb5c5fbfee895537574edf9162fb05fe831fd
Michael Murray <mdmurray@gmail.com>
2008-06-05 16:02:12 -0700

    Deploy on stage

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :application, "stage.apicaday"
 set :scm, :git
 set :user, "mik"
 set :repository,  "git@github.com:sudothinker/pic-a-day.git"
-set :branch, "origin/rel-1.1.1"
+set :branch, "origin/stage"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
