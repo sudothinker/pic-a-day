a537ed4646f0cbe13041af4e5ded1f79307d6b2c
Michael Murray <mdmurray@gmail.com>
2008-06-06 18:07:45 -0700

    Deploy scripts for rel-1.3

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
 set :keep_releases, 3
-set :application, "stage.apicaday"
+set :application, "apicaday"
 
 set :scm, :git
 set :user, "mik"
 set :repository,  "git@github.com:sudothinker/pic-a-day.git"
-set :branch, "origin/stage"
+set :branch, "origin/rel-1.3"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -15,12 +15,12 @@ set :deploy_to, "/home/mik/www/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-set :location, 'stage.pseudothinker.com'
+set :location, 'pseudothinker.com'
 role :app, location
 role :web, location
 role :db,  location, :primary => true
 
-set :deploy_via, :copy
+set :deploy_via, :remote_cache
 set :runner, user
 
 after "deploy:symlink", "sudothinker_symlink_configs"
