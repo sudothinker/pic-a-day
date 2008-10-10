70f89df9b95944e96303c8d712bfa3924fd97d67
Michael Murray <mdmurray@gmail.com>
2008-10-09 17:13:57 -0700

    Putting a link in the mini-feed title

--- app/models/picaday_publisher.rb
+++ app/models/picaday_publisher.rb
@@ -10,10 +10,10 @@ class PicadayPublisher < Facebooker::Rails::Publisher
   def templatized_news_feed(user, picture)
     send_as :templatized_action
     from(user)
-    title_template "{actor} took a picture of themself today."
+    title_template %({actor} took a picture today using <a href="http://apps.facebook.com/apictureeveryday">A Picture Everyday</a>)
     title_data Hash.new()
     body_template("")#link_to("Take a picture of yourself with your webcam today", "http://apps.facebook.com/apictureeveryday"))
-    body_general(link_to("Take a picture of yourself with your webcam today", "http://apps.facebook.com/apictureeveryday"))
+    body_general("")#link_to("Take a picture of yourself with your webcam today", "http://apps.facebook.com/apictureeveryday"))
     body_data(Hash.new())
     image_1(picture.thumb.authenticated_s3_url)
     image_1_link("http://apps.facebook.com/apictureeveryday/pictures/#{picture.id}")
