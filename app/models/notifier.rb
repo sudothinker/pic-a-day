5ab7eaede16ad49f3c38232a46442fd95cbe5fbd
Michael Murray <mdmurray@gmail.com>
2008-10-09 18:04:36 -0700

    Fixing smtp and email addresses

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,6 +1,6 @@
 class Notifier < ActionMailer::Base
   def daily_digest(pictures)
-    @recipients = "mdmurray@gmail.com"
+    @recipients = "mdmurray@gmail.com, youngj@gmail.com,h@henryyao.com"
     @from       = 'Picaday <noreply@apictureeveryday.com>'
     @subject    = "Pic a day Daily Digest: #{pictures.size} pictures"
     @sent_on    = Time.now
