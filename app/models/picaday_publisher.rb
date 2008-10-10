cc5164efb9d03a5511b162aeae9ddd325607262a
Michael Murray <mdmurray@gmail.com>
2008-10-09 17:02:33 -0700

    trying more template types

--- app/models/picaday_publisher.rb
+++ app/models/picaday_publisher.rb
@@ -11,9 +11,9 @@ class PicadayPublisher < Facebooker::Rails::Publisher
     send_as :templatized_action
     from(user)
     title_template "{actor} took a picture of themself today."
-    title_data(:actor => user.name)
-    body_template("")
-    body_general(link_to("Take a picture of yourself with your webcam today", "http://apps.facebook.com/apictureeveryday"))
+    title_data Hash.new()
+    body_template(link_to("Take a picture of yourself with your webcam today", "http://apps.facebook.com/apictureeveryday"))
+    #body_general(link_to("Take a picture of yourself with your webcam today", "http://apps.facebook.com/apictureeveryday"))
     body_data(Hash.new())
     image_1(picture.thumb.authenticated_s3_url)
     image_1_link("http://apps.facebook.com/apictureeveryday/pictures/#{picture.id}")
