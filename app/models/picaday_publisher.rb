8b0c8dd4dfd762476b212acc8d52a9b36cb72a26
Michael Murray <mdmurray@gmail.com>
2008-10-09 17:35:00 -0700

    Linking to user pic

--- app/models/picaday_publisher.rb
+++ app/models/picaday_publisher.rb
@@ -10,8 +10,8 @@ class PicadayPublisher < Facebooker::Rails::Publisher
   def templatized_news_feed(user, picture)
     send_as :templatized_action
     from(user)
-    title_template %({actor} took a picture today using <a href="http://apps.facebook.com/apictureeveryday">A Picture Everyday</a>)
-    title_data Hash.new()
+    title_template %({actor} took a <a href="http://apps.facebook.com/apictureeveryday/pictures/{picture}">picture</a> today. <a href="http://apps.facebook.com/apictureeveryday">Take yours</a>.)
+    title_data :picture => picture.id
     body_template("")#link_to("Take a picture of yourself with your webcam today", "http://apps.facebook.com/apictureeveryday"))
     body_general("")#link_to("Take a picture of yourself with your webcam today", "http://apps.facebook.com/apictureeveryday"))
     body_data(Hash.new())
